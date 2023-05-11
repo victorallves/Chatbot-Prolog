7c5f2c101a8931a023946e1c7097f51ba8205b0b
Victor Augusto <113953361+victorallves@users.noreply.github.com>
2023-05-10 21:01:16 -0300

    Add files via upload

---
+++ chatbot.pl
@@ -0,0 +1,94 @@
+﻿% Base de dados 1
+
+pergunta_resposta('O que é uma rede de computadores?', 'Uma rede de computadores é um conjunto de dispositivos eletrônicos interconectados que são capazes de trocar informações entre si.').
+
+pergunta_resposta('Qual a classificação de redes?', 'As redes podem ser classificadas em diversas categorias, como LAN, MAN e WAN, dependendo de sua extensão e do tipo de conexão utilizada.').
+
+pergunta_resposta('O que é um roteador?', 'Um roteador é um dispositivo que interconecta diferentes redes de computadores, encaminhando pacotes de dados entre elas.').
+
+pergunta_resposta('O que é uma placa de rede?', 'Uma placa de rede é um componente de hardware que permite que um computador se conecte a uma rede.').
+
+pergunta_resposta('O que é um Modem?', 'Um modem é um dispositivo que converte sinais digitais em sinais analógicos e vice-versa, permitindo a comunicação entre dispositivos em redes distintas.').
+
+pergunta_resposta('O que é um repetidor?', 'Um repetidor é um dispositivo que amplifica e retransmite sinais em uma rede de computadores, permitindo que estes alcancem maiores distâncias.').
+
+pergunta_resposta('O que é um Switch?', 'Um switch é um dispositivo que conecta diferentes dispositivos em uma rede de computadores, permitindo a comunicação entre eles.').
+
+pergunta_resposta('O que é uma LAN?', 'LAN é a sigla para Local Area Network, uma rede de computadores que abrange uma área geográfica limitada, como uma empresa, campus universitário ou edifício.').
+
+pergunta_resposta('O que é uma MAN?', 'MAN é a sigla para Metropolitan Area Network, uma rede de computadores que abrange uma área geográfica maior que uma LAN, como uma cidade ou região metropolitana.').
+
+pergunta_resposta('O que é uma WAN?', 'WAN é a sigla para Wide Area Network, uma rede de computadores que abrange uma área geográfica ainda maior que uma MAN, como um país ou continente.').
+
+pergunta_resposta('O que é internet?', 'A internet é uma rede de computadores mundial que permite a comunicação e o compartilhamento de informações entre seus usuários.').
+
+pergunta_resposta('O que é Correio eletrônico?', 'O Correio eletrônico, ou e-mail, é um serviço que permite enviar e receber mensagens de texto, imagens e outros tipos de arquivos pela internet.').
+
+pergunta_resposta('O que é um Cabo coaxial?', 'O cabo coaxial é um tipo de cabo utilizado para transmitir sinais de alta frequência, como os usados em redes de televisão e internet.').
+
+pergunta_resposta('O que é um Cabo de par trançado?', 'O cabo de par trançado é um tipo de cabo utilizado em redes de computadores para transmitir sinais de dados em alta velocidade.').
+
+pergunta_resposta('O que é a Fibra óptica?', 'A Fibra óptica é um tipo de cabo utilizado em redes de computadores que transmite sinais de luz para transmitir dados em alta velocidade e com baixa perda de sinal.').
+
+pergunta_resposta('O que é Radiofrequência?', 'Radiofrequência é um tipo de radiação eletromagnética que é usada em muitas aplicações, incluindo comunicações sem fio, transmissão de televisão e rádio, e equipamentos médicos e científicos.').
+
+pergunta_resposta('O que são Topologias de redes?', 'Topologia de rede refere-se ao layout físico ou lógico de uma rede de computadores. Existem vários tipos de topologias de rede, incluindo parcialmente ligada, em barramento, ponto a ponto, multiponto e em estrela.').
+
+pergunta_resposta('O que é uma Topologia Parcialmente Ligada?', 'Uma topologia parcialmente ligada é uma combinação de topologias em estrela e em barramento. Nesse tipo de topologia, vários dispositivos são conectados a um dispositivo central (como um hub), que é então conectado a outros dispositivos ou hubs na rede. Essa topologia é comumente usada em grandes redes empresariais.').
+
+pergunta_resposta('O que é uma Topologia em Barramento?', 'Uma topologia em barramento é uma rede na qual todos os dispositivos são conectados a um único cabo de comunicação, que é conhecido como barramento. Os dados são transmitidos em ambas as direções ao longo do cabo, e cada dispositivo recebe todas as transmissões. Essa topologia é comumente usada em redes de pequeno porte.').
+
+pergunta_resposta('O que é uma Topologia Ponto a Ponto?', 'Uma topologia ponto a ponto é uma rede na qual cada dispositivo está diretamente conectado a outro dispositivo na rede. Os dados são transmitidos de forma direta entre os dispositivos, sem a necessidade de um dispositivo central ou hub. Essa topologia é comumente usada em redes de pequeno porte e em aplicações de jogos online ou de compartilhamento de arquivos.').
+
+pergunta_resposta('O que é uma Topologia Multiponto?', 'Uma topologia multiponto é uma rede na qual vários dispositivos são conectados a um único dispositivo central, como um hub ou switch. Os dados são transmitidos entre os dispositivos e o dispositivo central, permitindo que os dispositivos se comuniquem uns com os outros. Essa topologia é comumente usada em redes de tamanho médio e grande.').
+
+pergunta_resposta('O que é uma Topologia em Estrela?', 'Uma topologia em estrela é uma rede na qual todos os dispositivos são conectados a um único dispositivo central, conhecido como hub ou switch. Os dados são transmitidos entre os dispositivos e o dispositivo central, permitindo que os dispositivos se comuniquem uns com os outros. Essa topologia é comumente usada em redes de tamanho médio e grande, pois oferece alta confiabilidade e facilidade de gerenciamento.').
+
+
+% Base de dados 2
+
+pergunta_resposta('O que é Ethernet?', 'Ethernet é uma tecnologia de rede local (LAN) que utiliza cabos físicos para conectar dispositivos em uma rede, permitindo a transmissão de dados de alta velocidade.').
+
+pergunta_resposta('O que é Endereçamento na ethernet e quais são os seus serviços?', 'O endereçamento na Ethernet é usado para identificar dispositivos em uma rede local e permite que as mensagens sejam direcionadas ao dispositivo correto. Os serviços de endereçamento na Ethernet incluem o endereço MAC e o endereço IP.').
+
+pergunta_resposta('O que é um Protocolo MAC?', 'O Protocolo MAC (Media Access Control) é um protocolo de rede que permite que vários dispositivos compartilhem o mesmo meio físico de transmissão em uma rede Ethernet.').
+
+pergunta_resposta('O que é Padrão Gigabit Ethernet?', 'O Padrão Gigabit Ethernet é uma versão da Ethernet que oferece uma velocidade de transmissão de 1 gigabit por segundo (Gbps), permitindo uma transmissão de dados mais rápida do que as versões anteriores da Ethernet.').
+
+pergunta_resposta('O que é Metro Ethernet?', 'Metro Ethernet é uma tecnologia de rede de alta velocidade que utiliza a infraestrutura Ethernet para conectar empresas e outras organizações a uma rede de área metropolitana (MAN).').
+
+pergunta_resposta('O que é o protocolo TCP/IP?', 'TCP/IP é um conjunto de protocolos de comunicação de rede que são usados para conectar dispositivos em uma rede, incluindo a internet. O TCP/IP é composto por dois protocolos principais: o TCP (Transmission Control Protocol) e o IP (Internet Protocol).').
+
+pergunta_resposta('Quais são os protocolos que constituem o TCP?', 'O TCP (Transmission Control Protocol) é composto pelos seguintes protocolos: IP (Internet Protocol), ARP (Address Resolution Protocol), ICMP (Internet Control Message Protocol), e UDP (User Datagram Protocol).').
+
+pergunta_resposta('O que é IGRP?', 'IGRP (Interior Gateway Routing Protocol) é um protocolo de roteamento usado em redes de área local (LANs) que permite que os dispositivos na rede troquem informações de roteamento e determinem a melhor rota para enviar dados.').
+
+pergunta_resposta('O que é EIGRP?', 'EIGRP (Enhanced Interior Gateway Routing Protocol) é um protocolo de roteamento usado em redes de área local (LANs) e em redes de área ampla (WANs) que permite que os dispositivos na rede troquem informações de roteamento e determinem a melhor rota para enviar dados.').
+
+pergunta_resposta('O que é SMTP?', 'SMTP (Simple Mail Transfer Protocol) é um protocolo de correio eletrônico usado para enviar mensagens de e-mail entre servidores de e-mail na internet.').
+
+pergunta_resposta('O que é Telnet?', 'Telnet é um protocolo de rede que permite que um usuário acesse e controle um dispositivo remoto através de uma conexão de rede.').
+
+pergunta_resposta('O que é FTP?', 'FTP (File Transfer Protocol) é um protocolo de transferência de arquivos que permite que os usuários transfiram arquivos entre dispositivos em uma rede, incluindo a internet.').
+
+pergunta_resposta('O que é http?', 'HTTP (Hypertext Transfer Protocol) é um protocolo de comunicação de rede que permite que os clientes (navegadores da web) se comuniquem com servidores web para solicitar e receber páginas da web.').
+
+pergunta_resposta('O que é camada de rede?', 'A camada de rede é a terceira camada do modelo de referência OSI (Open Systems Interconnection) e é responsável pelo encaminhamento dos pacotes de dados entre redes diferentes, permitindo a comunicação entre dispositivos em redes diferentes.').
+
+pergunta_resposta('O que é IPv4?', 'IPv4 (Internet Protocol version 4) é a quarta versão do protocolo de internet e é o protocolo mais comumente usado para conectar dispositivos à internet. O IPv4 usa endereços IP de 32 bits e pode suportar até 4,3 bilhões de endereços únicos.').
+
+pergunta_resposta('O que é ICMP?', 'ICMP (Internet Control Message Protocol) é um protocolo de rede que é usado para controlar e gerenciar o tráfego de rede, incluindo a detecção de erros e a solução de problemas de rede. O ICMP é usado para enviar mensagens de erro e controle entre dispositivos em uma rede.').
+
+
+main :-
+    repeat,
+    write('Qual é a sua pergunta?'), nl,
+    write('Digite tchau para terminar!'), nl,
+    read(Pergunta),
+    (   Pergunta == tchau
+    ->  write('Até mais!'), nl, !
+    ;   pergunta_resposta(Pergunta, Resposta)
+    ->  write(Resposta), nl
+    ;   write('Desculpe, não entendi a pergunta. Tente novamente.'), nl
+    ),
+    fail.
